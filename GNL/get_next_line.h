a290daf0af841474cad2f3d113eeecb0b609cd8f
Boyoung Kim <kimbbo1002@gmail.com>
2025-11-21 14:40:01 +0100

    starting gnl

---
+++ GNL/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bokim <bokim@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/21 12:45:03 by bokim             #+#    #+#             */
+/*   Updated: 2025/11/21 14:28:44 by bokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+//standard header files
+#include <unistd.h>
+#include <stdlib.h>
+
+//mandatory functions
+char	*get_next_line(int fd);
+
+
+#endif
